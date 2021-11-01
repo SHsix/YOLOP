2df17fbf234fe9ca952bf42531969272ce59e7da
Hsix <shinhyeonsik@gmail.com>
2021-11-01 17:33:09 +0900

    fix

--- lib/core/function.py
+++ lib/core/function.py
@@ -489,7 +489,6 @@ class AverageMeter(object):
 import torch, os, datetime
 import numpy as np
 
-from model.model import parsingNet
 from data.dataloader import get_train_loader
 
 from utils.dist_utils import dist_print, dist_tqdm, is_main_process, DistSummaryWriter
