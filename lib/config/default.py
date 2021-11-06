d74ff0af69dc07475e038d8838269297b50e1d0f
Hsix <shinhyeonsik@gmail.com>
2021-11-06 14:58:44 +0900

    fix

--- lib/config/default.py
+++ lib/config/default.py
@@ -8,7 +8,7 @@ _C.LOG_DIR = 'runs/'
 _C.GPUS = (0, 1)
 _C.WORKERS = 8
 _C.PIN_MEMORY = False
-_C.PRINT_FREQ = 20
+_C.PRINT_FREQ =40
 _C.AUTO_RESUME = False       # Resume from the last training interrupt
 # Re-select the prior anchor(k-means)    When training from scratch (epoch=0), set it to be ture!
 _C.NEED_AUTOANCHOR = False
@@ -103,7 +103,7 @@ _C.TRAIN.BEGIN_EPOCH = 0
 _C.TRAIN.END_EPOCH = 2
 
 _C.TRAIN.VAL_FREQ = 1
-_C.TRAIN.BATCH_SIZE_PER_GPU = 36
+_C.TRAIN.BATCH_SIZE_PER_GPU = 30
 _C.TRAIN.SHUFFLE = True
 
 _C.TRAIN.IOU_THRESHOLD = 0.2
