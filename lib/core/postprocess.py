8674706542243b0474e24707a29b63d6155a935e
Hsix <shinhyeonsik@gmail.com>
2021-11-06 14:45:34 +0900

    fix

--- lib/core/postprocess.py
+++ lib/core/postprocess.py
@@ -39,7 +39,6 @@ def build_targets(cfg, predictions, targets, model):
     
     for i in range(det.nl):
         anchors = det.anchors[i] #[3,2]
-        print(predictions[i].shape)
         gain[2:6] = torch.tensor(predictions[i].shape)[[3, 2, 3, 2]]  # xyxy gain
         # Match targets to anchors
         t = targets * gain
