a4376dc2c09dc4d264c8dfe70788d91869778b61
Hsix <shinhyeonsik@gmail.com>
2021-11-07 19:21:52 +0900

    default

--- lib/core/function.py
+++ lib/core/function.py
@@ -506,7 +506,7 @@ def inference(net, data_label, use_aux):
         img, cls_label, seg_label = data_label
         img, cls_label, seg_label = img.cuda(), cls_label.long().cuda(), seg_label.long().cuda()
         cls_out, seg_out = net(img)[1]
-  
+        print(cls_out.shape, seg_out.shape)
         return {'cls_out': cls_out, 'cls_label': cls_label, 'seg_out':seg_out, 'seg_label': seg_label}
     else:
         img, cls_label = data_label
